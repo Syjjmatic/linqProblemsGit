ef82d771a3671c822a4876a992a41446ef9ca169
Andrew Curzon <andrew.w.curzon@gmail.com>
2020-03-09 10:59:13 -0500

    Created ProblemOne class and create a new List variable called words.

---
+++ linqProblems/ProblemOne.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linqProblems
+{
+    class ProblemOne
+    {
+        List<string> words;
+        public ProblemOne()
+        {
+            new List<string>() { "Mike", "Brad", "Ian", "Mike" };
+        }
+    }
+}
