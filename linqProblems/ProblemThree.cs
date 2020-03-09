416ed47d7cd5a1d975022936da720c962caefe63
Andrew Curzon <andrew.w.curzon@gmail.com>
2020-03-09 13:37:21 -0500

    saving progress

--- linqProblems/ProblemThree.cs
+++ linqProblems/ProblemThree.cs
@@ -8,9 +8,15 @@ namespace linqProblems
 {
     class ProblemThree
     {
+        List<string> classGrades;
+
         public ProblemThree()
         {
-
+            classGrades = new List<string>
+            {"80, 100, 92, 89, 65",
+            "93, 81, 78, 84, 69",
+            "73, 88, 83, 99, 64",
+            "98, 100, 66, 74, 55"};
         }
 
         public void Run()
